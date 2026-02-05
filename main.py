51f4a5ac343a4e96a6449cc0d2cf29979bf68f47
Lucas <lucas.donati@tohu.ch>
2026-02-06 00:30:10 +0100

    modifying path

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import subprocess
 import sys
+import os
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC  
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+#from webdriver_manager.firefox import GeckoDriverManager
 
 channel_name = "eteicos_"
+os.environ["PATH"] += os.pathsep + "/bin/"
 
 options = webdriver.FirefoxOptions()
 options.add_argument("-headless")
-#driver = webdriver.Firefox(options=options)
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+#options.binary_location = "/bin/firefox"
+driver = webdriver.Firefox(options=options)
+#driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 
 driver.get(f"https://www.twitch.tv/{channel_name}/videos?filter=archives&sort=time")
 #print(driver.current_url)
