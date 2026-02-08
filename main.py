130b29e586fad27fa0c1b7f600db47191d27948c
Lucas <lucas.donati@tohu.ch>
2026-02-09 00:22:28 +0100

    cleanup + output dir option

--- main.py
+++ main.py
@@ -10,24 +10,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
 # Parsing command line arguments
-options, arguments = getopt.getopt(os.sys.argv[1:], "c:", ["channel="])
+options, arguments = getopt.getopt(os.sys.argv[1:], "c:o:", ["channel=", "output="])
 for option, argument in options:
     if option in ("-c", "--channel"):
         channel_name = argument
+    if option in ("-o", "--output"):
+        output_dir = argument
 
 if not channel_name:
     print("Error: Twitch channel name is required. Use -c or --channel to specify it.", file=sys.stderr)
     sys.exit(1)
 
-#os.environ["PATH"] += os.pathsep + "/bin/"
 
 options = webdriver.FirefoxOptions()
 options.add_argument("-headless")
-#options.binary_location = "/bin/firefox"
 driver = webdriver.Firefox(options=options)
 
 driver.get(f"https://www.twitch.tv/{channel_name}/videos?filter=archives&sort=time")
-#print(driver.current_url)
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.TAG_NAME, "article"))
 )
@@ -42,7 +41,6 @@ driver.quit()
 curr_url_essential = curr_url.split("?")[0]
 video_id = curr_url_essential.split("/")[-1]
 
-#print(curr_url_essential)
 print("ID of last video as detected:")
 print(video_id)
 
@@ -57,12 +55,12 @@ except FileNotFoundError:
     print("No record of last video ID found. Assuming this is the first run.")
     last_video_id = None
 
-command = ["pipenv", "run", "yt-dlp", curr_url_essential, "-o", str(date.today()), "-t", "mkv"]
+full_path = os.path.join(output_dir, str(date.today()))
+command = ["pipenv", "run", "yt-dlp", curr_url_essential, "-o", full_path, "-t", "mkv"]
 flag_run = False
 
 if last_video_id != video_id:
     try:
-        #subprocess.run(command, check=True, capture_output=True, text=True)
         subprocess.run(command, check=True, text=True)
         flag_run = True
     except subprocess.CalledProcessError as e:
