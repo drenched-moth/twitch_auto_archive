e90ea418d13402ef2a072edd0b174b2b36bc3c4c
Lucas <lucas.donati@tohu.ch>
2026-02-11 20:08:15 +0100

    Many improvements
    - Using stream date, not script execution date as filename
    - Checking stream time to not download before stream has ended
    - Using python API for yt-dlp
    - Using 2 cores for downloading

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import subprocess
+import yt_dlp
 import shutil
 import sys
-from datetime import date
+from datetime import date, datetime
 import os
 import getopt
 from selenium import webdriver
@@ -11,17 +12,20 @@ from selenium.webdriver.common.by import By
 #from selenium.webdriver.firefox.service import Service as FirefoxService
 
 DEFAULT_OUTPUT_DIR = "./"
-FORMAT = "mkv"
+FORMAT = "mp4"
+format = FORMAT
 output_dir = DEFAULT_OUTPUT_DIR
 channel_name = None
 
 # Parsing command line arguments
-options, arguments = getopt.getopt(os.sys.argv[1:], "c:o:", ["channel=", "output="])
+options, arguments = getopt.getopt(os.sys.argv[1:], "c:o:f:", ["channel=", "output=", "format="])
 for option, argument in options:
     if option in ("-c", "--channel"):
         channel_name = argument
     if option in ("-o", "--output"):
         output_dir = argument
+    if option in ("-f", "--format"):
+        format = argument
 
 if not channel_name:
     print("Error: Twitch channel name is required. Use -c or --channel to specify it.", file=sys.stderr)
@@ -42,6 +46,32 @@ WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "ScMediaCardStatWrapper-sc-anph5i-0"))
 )
 
+links_to_videos = driver.find_elements(By.TAG_NAME, "article")
+partial_link = links_to_videos[0].find_element(By.TAG_NAME, "a").get_attribute("href")
+curr_url = partial_link
+
+print(f"Partial url detected as last video of user \"{channel_name}\": {curr_url}")
+links_to_videos = driver.find_elements(By.TAG_NAME, "article")
+
+curr_url_essential = curr_url.split("?")[0]
+video_id = curr_url_essential.split("/")[-1]
+
+print(f"ID of last video as detected: {video_id}")
+
+last_video_id_path = "./last_video_id"
+
+try:
+    with open(last_video_id_path, "r") as f:
+        last_video_id = f.readline().strip()
+        print(f"ID of last video downloaded: {last_video_id}")
+except FileNotFoundError:
+    print("No record of last video ID found. Assuming this is the first run.")
+    last_video_id = None
+
+if last_video_id == video_id:
+    print("Last video ID matches the most recent video. No new video to download.")
+    driver.quit()
+    sys.exit(0)
 
 ## Avant de démarrer le download on veut vérifier que le live est terminé
 ## pour cela on peut sauvegarder la longueur de la vidéo et vérifier qu'elle n'a pas changé après quelques secondes (à voir s'il faut faire un sleep ou pas)
@@ -53,30 +83,17 @@ print(f"Length of last video detected as: {video_length1}")
 
 driver.get(driver.current_url)
 
-#WebDriverWait(driver, 10).until(
-#    EC.presence_of_element_located((By.TAG_NAME, "article"))
-#)
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "ScMediaCardStatWrapper-sc-anph5i-0"))
 )
 
-
-
-video_length2 = driver.find_elements(By.TAG_NAME, "article")[0].find_element(By.CLASS_NAME, "ScMediaCardStatWrapper-sc-anph5i-0").text
+links_to_videos = driver.find_elements(By.TAG_NAME, "article")
+video_length2 = links_to_videos[0].find_element(By.CLASS_NAME, "ScMediaCardStatWrapper-sc-anph5i-0").text
 print(f"Length of last video detected as: {video_length2}")
 
-links_to_videos = driver.find_elements(By.TAG_NAME, "article")
-partial_link = links_to_videos[0].find_element(By.TAG_NAME, "a").get_attribute("href")
-curr_url = partial_link
-
-print(f"Partial url detected as last video of user \"{channel_name}\": {curr_url}")
-links_to_videos = driver.find_elements(By.TAG_NAME, "article")
-
-#partial_link = links_to_videos[0].find_element(By.TAG_NAME, "a").get_attribute("href")
-#curr_url = partial_link
-
-#curr_url = driver.current_url
-#print(f"Partial url detected as last video of user \"{channel_name}\": {curr_url}")
+stream_date = links_to_videos[0].find_elements(By.TAG_NAME, "img")[-1].get_attribute("title")
+stream_date_obj = datetime.strptime(stream_date, "%b %d, %Y").date()
+print(f"Found date of last video: {stream_date} -> {stream_date_obj}")
 
 driver.quit()
 if video_length1 != video_length2:
@@ -84,42 +101,27 @@ if video_length1 != video_length2:
     sys.exit(0)
 
 
-curr_url_essential = curr_url.split("?")[0]
-video_id = curr_url_essential.split("/")[-1]
-
-print("ID of last video as detected:")
-print(video_id)
-
-last_video_id_path = "./last_video_id"
-
-try:
-    with open(last_video_id_path, "r") as f:
-        last_video_id = f.readline().strip()
-        print("ID of last video downloaded:")
-        print(last_video_id)
-except FileNotFoundError:
-    print("No record of last video ID found. Assuming this is the first run.")
-    last_video_id = None
-
-#full_path = os.path.join(output_dir, str(date.today()))
-filename = f"{date.today()}.{FORMAT}"
+filename = f"{stream_date_obj}.{format}"
 full_path = os.path.join(output_dir, filename)
 print(f"Temporary filename for downloaded video: {filename}")
 print(f"Full path for output file after finished download: {full_path}")
-command = ["pipenv", "run", "yt-dlp", curr_url_essential, "-o", str(date.today()), "-t", FORMAT]
+
+ydl_opts = {
+    'outtmpl': {'default': full_path},
+    'concurrent_fragment_downloads': 2,           
+}
 flag_run = False
 
 if last_video_id != video_id:
     try:
-        subprocess.run(command, check=True, text=True)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([curr_url])
         flag_run = True
-    except subprocess.CalledProcessError as e:
+    except Exception as e:
         print("Download failed. Will not update last video ID.")
         print(f"Error downloading video: {e}", file=sys.stderr)
-        print(f"STDOUT: {e.stdout}", file=sys.stdout)
-        print(f"STDERR: {e.stderr}", file=sys.stderr)
 
 if flag_run:
     with open(last_video_id_path, "w") as f:
         f.write(video_id)
-    shutil.move(f"./{filename}", full_path)
+#    shutil.move(f"./{filename}", full_path)
