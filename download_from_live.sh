58682c78b767020fd53a637bb2d6a402d4c29298
Lucas <lucas.donati@tohu.ch>
2026-03-12 01:36:23 +0100

    wrapper, segmentation and using youtubeuploader

--- download_from_live.sh
+++ download_from_live.sh
@@ -5,32 +5,36 @@ set -euo pipefail
 cleanup() {
 	rm -rf "$tmpdir"
 }
+SCRIPT_NAME=$(basename "$0")
+source "$(dirname "$0")/log.sh"
 
 channel="${1:-}"
 output_path="${2:-}"
-if [ -z "$channel" ] || [ -z "$output_path" ]; then
-	echo "Usage: $0 <channel_name> <output_path>"
+tmpdir="${3:-}"
+if [ -z "$channel" ] || [ -z "$output_path" ] || [ -z "$tmpdir" ]; then
+	echo "Usage: $0 <channel_name> <output_path> <tmpdir>"
 	exit 1
 fi
 
 channel_info_file="id_$channel"
 if [ ! -f "$channel_info_file" ] ; then
-	echo "Getting id of channel $channel"
+	log "Getting id of channel $channel"
 	# Getting id of channel (necessary for videos api call)
 	raw_data=$(./twitch api get /users -q login=$channel)
 	if echo "$raw_data" | jq -e 'isempty(.data[])' >/dev/null ; then
-		echo "Error fetching user's id" 
+		log "Error fetching user's id" 
 		exit 1
 	fi
 	echo "$raw_data" > "$channel_info_file"
 else
+	log "Using cached info for channel $channel"
 	raw_data=$(cat "$channel_info_file")
 fi
 
 user_id=$(echo "$raw_data" | jq '.data[].id | tonumber')
 current_download_file="downloading_$user_id"
 
-echo -n "Checking if $channel is live : "
+log -n "Checking if $channel is live : "
 # If currently live -> download
 [ $(./twitch api get /streams -q user_id=$user_id | jq 'isempty(.data[])') = true ] && echo "Not currently streaming" && exit 1
 echo -n "Currently streaming"
@@ -40,7 +44,7 @@ echo " starting download of current stream from the beginning"
 # Getting channel's last stream data
 raw_data=$(./twitch api get /videos -q type=archive -q first=1 -q user_id=$user_id)
 if echo "$raw_data" | jq -e 'isempty(.data[])' >/dev/null ; then
-	echo "Error fetching last stream's data"
+	log "Error fetching last stream's data"
 	exit 1
 fi 
 data=$(echo $raw_data | jq '.data[]')
@@ -48,19 +52,17 @@ video_id=$(echo $data | jq '.id | tonumber')
 title=$(echo $data | jq -r '.title')
 creation_date=$(echo $data | jq -r '.created_at | split("T")[0]')
 
-echo "Title=$title"
-echo "Date=$creation_date"
-echo "ID=$video_id"
+log "Title=$title"
+log "Date=$creation_date"
+log "ID=$video_id"
 
-[ ! -f last_video_id ] && echo 0 > last_video_id && echo "Not any last video was detected"
-[ $(cat last_video_id) = $video_id ] && echo "Last video detected is the same as last downloaded" && exit 1
+[ ! -f last_video_id ] && echo 0 > last_video_id && log "Not any last video was detected"
+[ $(cat last_video_id) = $video_id ] && log "Last video detected is the same as last downloaded" && exit 1
 
-# Setup before downloading new video
-tmpdir=$(mktemp -d -t twitch-archive-XXXXXX)
 
-output_path="$output_path/$channel"
-archive_dir="$output_path/$creation_date""_$video_id"
-mkdir -p "$archive_dir"
+# output_path="$output_path/$channel"
+# archive_dir="$output_path/$creation_date""_$video_id"
+# mkdir -p "$archive_dir"
 
 # filename is actually path + filename
 filename=$(~/.local/bin/yt-dlp https://twitch.tv/videos/$video_id --print filename --progress-delta 15 --newline -o "$tmpdir/video.%(ext)s")
@@ -68,10 +70,11 @@ ext="${filename##*.}"
 echo "$channel" > "$current_download_file"
 ~/.local/bin/yt-dlp https://twitch.tv/videos/$video_id --live-from-start --progress-delta 15 --no-part --newline --verbose -o "$tmpdir/video.%(ext)s" 
 echo "$video_id" > last_video_id 
-echo "$data" > "$archive_dir/metadata.json"
-mv "$filename" "$archive_dir/video.$ext" -v
-rm -f "$current_download_file"
+# echo "$data" > "$archive_dir/metadata.json"
+echo "$data" > "$tmpdir/metadata.json"
+# mv "$filename" "$archive_dir/video.$ext" -v
+# rm -f "$current_download_file"
 
-echo "Stream archived in $archive_dir"
+log "All data in $tmpdir"
 
 trap cleanup EXIT
---
+++ wrapper.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+CHANNEL="${1:-}"
+UPLOAD_CHANNEL="${2:-}"
+OUTPUT_PATH="${3:-}"
+
+if [ -z "$CHANNEL" ] || [ -z "$OUTPUT_PATH" ] || [ -z "$UPLOAD_CHANNEL" ]; then
+    echo "Usage: $0 <channel_name> <upload_channel_name> <output_path>"
+    exit 1
+fi
+
+LOGFILE="$SCRIPT_DIR/archive.log"
+exec >>"$LOGFILE" 2>&1
+
+SCRIPT_NAME=$(basename "$0")
+source "$(dirname "$0")/log.sh"
+
+cleanup() {
+	rm -rf "$tmpdir"
+}
+
+log "Starting archive pipeline for $CHANNEL"
+tmpdir=$(mktemp -d -t twitch-archive-XXXXXX)
+
+# téléchargement
+log "Downloading latest VOD"
+log "$SCRIPT_DIR/download_from_live.sh" "$CHANNEL" "$OUTPUT_PATH" "$tmpdir"
+
+log "Download finished"
+
+data=$(cat "$tmpdir/metadata.json")
+video_id=$(echo $data | jq '.id | tonumber')
+title=$(echo $data | jq -r '.title')
+creation_date=$(echo $data | jq -r '.created_at | split("T")[0]')
+
+# upload
+log "Uploading VOD"
+# "$SCRIPT_DIR/upload-vod.sh" "$CHANNEL"
+./youtube-upload -filename "$tmpdir/video."* -secrets "$SCRIPT_DIR/client_secrets_$UPLOAD_CHANNEL.json" -cache "$SCRIPT_DIR/request_$UPLOAD_CHANNEL.token" -title "$title - $creation_date" -description "Archived Twitch stream from $creation_date with id $video_id"
+
+log "Upload finished"
+
+log "Moving data to archive directory"
+
+output_path="$OUTPUT_PATH/$CHANNEL"
+archive_dir="$output_path/$creation_date""_$video_id"
+mkdir -p "$archive_dir"
+mv -v "$tmpdir/"* "$archive_dir/"
+
+log "Pipeline completed"
+
+trap cleanup EXIT
