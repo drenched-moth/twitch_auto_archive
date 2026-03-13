0244b45c58d13aee2ee48077136e617ea1505e41
drenched_moth <lucas.donati@tohu.ch>
2026-03-13 10:51:11 +0100

    clean + script to download last video

---
+++ download_last_video.sh
@@ -0,0 +1,74 @@
+# Stop si erreur pas handled, erreur si var non def
+# pipefail arrête le script si cmd1 dans cmd1 | jq cause une erreur, évitant des comportement indet 
+set -euo pipefail
+
+SCRIPT_NAME=$(basename "$0")
+source "$(dirname "$0")/log.sh"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+channel="${1:-}"
+output_path="${2:-}"
+tmpdir="${3:-}"
+if [ -z "$channel" ] || [ -z "$output_path" ] || [ -z "$tmpdir" ]; then
+	echo "Usage: $0 <channel_name> <output_path> <tmpdir>"
+	exit 1
+fi
+
+channel_info_file="id_$channel"
+if [ ! -f "$channel_info_file" ] ; then
+	log "Getting id of channel $channel"
+	# Getting id of channel (necessary for videos api call)
+	raw_data=$("$SCRIPT_DIR"/twitch api get /users -q login=$channel)
+	if echo "$raw_data" | jq -e 'isempty(.data[])' >/dev/null ; then
+		log "Error fetching user's id" 
+		exit 1
+	fi
+	echo "$raw_data" > "$channel_info_file"
+else
+	log "Using cached info for channel $channel"
+	raw_data=$(cat "$channel_info_file")
+fi
+
+user_id=$(echo "$raw_data" | jq '.data[].id | tonumber')
+
+# If currently live -> download
+[ $("$SCRIPT_DIR"/twitch api get /streams -q user_id=$user_id | jq 'isempty(.data[])') = false ] && log "Currently streaming" && exit 1
+log "Not currently streaming"
+
+# Getting channel's last stream data
+raw_data=$("$SCRIPT_DIR"/twitch api get /videos -q type=archive -q first=1 -q user_id=$user_id)
+if echo "$raw_data" | jq -e 'isempty(.data[])' >/dev/null ; then
+	log "Error fetching last stream's data"
+	exit 1
+fi 
+data=$(echo $raw_data | jq '.data[]')
+video_id=$(echo $data | jq '.id | tonumber')
+title=$(echo $data | jq -r '.title')
+creation_date=$(echo $data | jq -r '.created_at | split("T")[0]')
+
+log "Title=$title"
+log "Date=$creation_date"
+log "ID=$video_id"
+
+[ ! -f "$channel"_last_video_id ] && echo 0 > "$channel"_last_video_id && log "Not any last video was detected"
+[ $(cat "$channel"_last_video_id) = $video_id ] && log "Last video detected is the same as last downloaded" && exit 1
+
+
+# output_path="$output_path/$channel"
+# archive_dir="$output_path/$creation_date""_$video_id"
+# mkdir -p "$archive_dir"
+
+# filename is actually path + filename
+filename=$(~/.local/bin/yt-dlp https://twitch.tv/videos/$video_id --print filename --progress-delta 15 --newline -o "$tmpdir/video.%(ext)s")
+ext="${filename##*.}"
+# ~/.local/bin/yt-dlp https://twitch.tv/videos/$video_id --live-from-start --progress-delta 15 --no-part --newline --verbose -o "$tmpdir/video.%(ext)s" 
+~/.local/bin/yt-dlp https://twitch.tv/videos/$video_id -N 4 --progress-delta 15 --no-part --newline -o "$tmpdir/video.%(ext)s" 
+echo "$video_id" > last_video_id 
+# echo "$data" > "$archive_dir/metadata.json"
+echo "$data" > "$tmpdir/metadata.json"
+# mv "$filename" "$archive_dir/video.$ext" -v
+
+log "All data in $tmpdir"
+
+#trap cleanup EXIT
